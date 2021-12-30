a506876756b0337a7236deadf5fb8a34fe3b106e
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-12-30 09:30:28 +0600

    login register

---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVZkf-UCA2LWOpaHdww2e59mxYRYSMPp0",
+  authDomain: "vangari-40ecc.firebaseapp.com",
+  projectId: "vangari-40ecc",
+  storageBucket: "vangari-40ecc.appspot.com",
+  messagingSenderId: "775065759657",
+  appId: "1:775065759657:web:5105d73e72aeed623bdeb8"
+};
+export default firebaseConfig;
